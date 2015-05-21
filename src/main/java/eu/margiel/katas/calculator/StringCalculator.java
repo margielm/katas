db463efab9bf35ea27c0108f3465b23b099f1b0e
Michal Margiel <Michal.Margiel@gmail.com>
2015-05-21 12:29:42 +0200

    StringCalculator: first step, after refactor

--- src/main/java/eu/margiel/katas/calculator/StringCalculator.java
+++ src/main/java/eu/margiel/katas/calculator/StringCalculator.java
@@ -1,11 +1,15 @@
 package eu.margiel.katas.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(final String numbers) {
         if (numbers.length() == 0) {
             return 0;
         }
-        return numbers.split(",").length;
+        return Arrays.stream(numbers.split(","))
+            .mapToInt(Integer::parseInt)
+            .sum();
     }
 }
--- src/test/groovy/eu/margiel/katas/calculator/StringCalculatorSpec.groovy
+++ src/test/groovy/eu/margiel/katas/calculator/StringCalculatorSpec.groovy
@@ -1,6 +1,7 @@
 package eu.margiel.katas.calculator
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class StringCalculatorSpec extends Specification {
     private def calculator = new StringCalculator()
@@ -11,14 +12,19 @@ class StringCalculatorSpec extends Specification {
           calculator.add("") == 0
     }
 
+    @Unroll
     def "should return same value for adding one number"() {
         expect:
-          calculator.add("1") == 1
+          calculator.add(numbers) == result
+        where:
+          numbers || result
+          "1"     || 1
+          "2"     || 2
+          "10"    || 10
     }
 
     def "should return sum of two numbers"() {
         expect:
           calculator.add("1,1") == 2
-
     }
 }
