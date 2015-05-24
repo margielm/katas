a416c99e276e1dc25efc985d12d564fae604f4d1
Michal Margiel <Michal.Margiel@gmail.com>
2015-05-24 13:59:08 +0200

    KataPotter: after refactoring

--- src/main/java/eu/margiel/katas/potter/CashRegister.java
+++ src/main/java/eu/margiel/katas/potter/CashRegister.java
@@ -1,5 +1,54 @@
 package eu.margiel.katas.potter;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
+
 public class CashRegister {
+
+    public static final BigDecimal BOOK_PRICE = new BigDecimal("8");
+    private Map<Integer, String> discounts = new HashMap<Integer, String>() {
+        {
+            put(1, "0");
+            put(2, "0.05");
+            put(3, "0.10");
+            put(4, "0.20");
+            put(5, "0.25");
+        }
+    };
+
+    public BigDecimal priceFor(List<Integer> cart) {
+        return discoverSetsIn(cart)
+            .stream()
+            .map(BigDecimal::new)
+            .reduce(ZERO, (price, set) ->
+                {
+                    BigDecimal setPrice = set.multiply(BOOK_PRICE).multiply(ONE.subtract(new BigDecimal(discounts.get(set.intValue()))));
+                    return price.add(setPrice);
+                }
+            );
+    }
+
+    private List<Integer> discoverSetsIn(List<Integer> cart) {
+        List<Integer> sets = new ArrayList<>();
+        int setIdx = 0;
+        while (!cart.isEmpty()) {
+            for (Integer book = 1; book <= 5; book++) {
+                if (cart.remove(book)) {
+                    if (sets.size() <= setIdx) {
+                        sets.add(0);
+                    }
+                    sets.set(setIdx, sets.get(setIdx) + 1);
+                }
+            }
+            setIdx++;
+        }
+        return sets;
+    }
 
 }
--- src/test/groovy/eu/margiel/katas/potter/CashRegisterSpec.groovy
+++ src/test/groovy/eu/margiel/katas/potter/CashRegisterSpec.groovy
@@ -1,9 +1,73 @@
 package eu.margiel.katas.potter
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
-/**
- * Created by margielm on 23.05.2015.
- */
 class CashRegisterSpec extends Specification {
+    def cashRegister = new CashRegister()
+
+    def "price should be 0 for empty cart"() {
+        expect:
+          cashRegister.priceFor([]) == 0.0
+    }
+
+    def "price should be 8 for one 1st book only"() {
+        expect:
+          cashRegister.priceFor([1]) == 8.0
+    }
+
+    @Unroll
+    def "should pay full price for 2 copies of same book"() {
+        expect:
+          cashRegister.priceFor(cart) == 16.0
+
+        where:
+          cart << [
+              [1, 1],
+              [2, 2],
+              [3, 3]
+          ]
+    }
+
+    def "should get 5% discount if buying 1st and 2nd book "() {
+        expect:
+          cashRegister.priceFor([1, 2]) == 15.2
+    }
+
+    def "should get 10% discount if buying 1st, 2nd and 3rd book "() {
+        expect:
+          cashRegister.priceFor([1, 2, 3]) == 21.6
+    }
+
+    def "should pay full price for 3 copies of same book"() {
+        expect:
+          cashRegister.priceFor([1, 1, 1]) == 24.0
+    }
+
+    def "should get 20% discount if buying 4 different books"() {
+        expect:
+          cashRegister.priceFor([1, 2, 3, 4]) == 4 * 8 * 0.8
+    }
+
+    def "should get 25% discount if buying 5 different books"() {
+        expect:
+          cashRegister.priceFor([1, 2, 3, 4, 5]) == 5 * 8 * 0.75
+    }
+
+    def "should get 5% discount for two copies of 1st and one 2nd"() {
+        expect:
+          cashRegister.priceFor([1, 1, 2]) == (8 + (8 + 8) * 0.95)
+    }
+
+    def "should get price for 2 sets of 2 books size"() {
+        expect:
+          cashRegister.priceFor([1, 2, 1, 2]) == 4 * 8 * 0.95
+    }
+
+    def "should get price for 2 sets of 2  and 1 set 3 books size"() {
+        expect:
+          cashRegister.priceFor([1, 2, 1, 2, 1, 2, 3]) == 4 * 8 * 0.95 + 3 * 8 * 0.9
+    }
+
+
 }
